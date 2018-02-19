dde06f4d39b46a75675d6b2c78bf2865325557ce
Simon Detheridge <simon@widgit.com>
2018-02-19 10:05:25 +0000

    Add bridge-utils for configuring docker net

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -7,6 +7,7 @@ echo "app-emulation/docker ~amd64" > /etc/portage/package.accept_keywords/docker
 echo "app-emulation/docker overlay -device-mapper" > /etc/portage/package.use/docker
 
 emerge app-emulation/docker
+emerge net-misc/bridge-utils
 
 systemctl enable docker.service
 usermod -aG docker vagrant
